ef91e5c03496ec4e069f7df6b4f52f89c713652a
ismaelpaiva <ismaelpaivam@gmail.com>
2017-06-08 23:04:14 -0300

    Resolvendo um problema

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -2,5 +2,20 @@
 
 class CadastroController extends \HXPHP\System\Controller
 {
+
+	public function cadastrarAction()
+	{
+		$this->view->setFile('index');
+
+		$this->request->setCustomFilters(array(
+			'email' => FILTER_VALIDATE_EMAIL
+
+		));
+
+		var_dump($this->request->post());
+
+		//Gerar Senha
+		//Obter Role_id
+	}	
 	
 }
